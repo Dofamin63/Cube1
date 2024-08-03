786fcc48001096921bce7746272089df0f62abb2
Beloskov <88579232+Dofamin63@users.noreply.github.com>
2024-08-03 19:58:30 +0700

    commit

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    
-    void Update()
+
+    private void Update()
     {
         transform.position += transform.forward * (_speed * Time.deltaTime);
     }
--- Assets/Scripts/Scale.cs
+++ Assets/Scripts/Scale.cs
@@ -1,11 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Scale : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    void Update()
+
+    private void Update()
     {
         transform.localScale += Vector3.one * (_speed * Time.deltaTime);
     }
